848c70895c30762a9c397e8e4dba778c666da561
Geoffrey Choy <geoffchoy@outlook.com>
2025-06-16 12:16:31 +0100

    Fix PRED parameter for data period

--- data_source/fred.py
+++ data_source/fred.py
@@ -7,19 +7,30 @@ import requests
 
 API_BASE_URL = "https://api.stlouisfed.org"
 
+ # lookup: https://fred.stlouisfed.org/series/<code>
 series_id_map = {
-    # https://fred.stlouisfed.org/series/UMCSENT
     "UMCSENT": "University of Michigan: Consumer Sentiment",
-    # https://fred.stlouisfed.org/series/PERMIT
     "PERMIT": "New Privately-Owned Housing Units Authorized in Permit-Issuing Places: Total Units",
+    "HOUST": "Housing Starts",
+    "COMPU1USA": "Housing Completions",
+    "ICSA": "Initial Jobless Claims",
+    "CCSA": "Continuing Claims",
+    "MICH": "USMCI: Inflation Expectations",
+    "RSXFS": "Advanced Retail Sales",
+    "DGORDER": "Durable Goods Orders",
+    "ACDGNO": "New Orders: Total Manufacturing",
+    "DPI": "Disposable Personal Income",
+    "IEABC": "Capital Account (Quarterly)",
+    "IEABCP": "Capital Account (CA) (Quarterly)",
+    "PCE": "Personal Consumption Expenditure (PCE)",
 }
 
 def get_series_data(api_key: str, series_id: str, start_date: str, end_date: str, freq: str):
     params = {
         "api_key": api_key,
         "series_id": series_id,
-        "realtime_start": start_date,
-        "realtime_end": end_date,
+        "observation_start": start_date,
+        # "realtime_end": end_date, # defaults to 9999-12-31 (latest available)
         "frequency": freq,
         "sort_order": "desc",
         "file_type": "json"
